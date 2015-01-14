1e6db32418a9e1ee2c705ffb525176fbe178205a
Gapur <gapur.kassym@gmail.com>
2015-01-15 02:36:59 +0700

    building master page layout

--- Store/Store/App_Start/BundleConfig.cs
+++ Store/Store/App_Start/BundleConfig.cs
@@ -8,24 +8,29 @@ namespace Store
         //Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jsfw/jquery").Include(
+                        "~/Scripts/jsfw/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jsfw/jqueryval").Include(
+                        "~/Scripts/jsfw/jquery.validate*"));
 
             // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
             // используйте средство сборки на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/jsfw/modernizr").Include(
+                        "~/Scripts/jsfw/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jsfw/bootstrap").Include(
+                      "~/Scripts/jsfw/bootstrap.js",
+                      "~/Scripts/jsfw/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jsfw/knockout").Include(
+                     "~/Scripts/jsfw/knockout-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css/css").Include(
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css",
+                      "~/Content/css/Main.css",
+                      "~/Content/css/simple-sidebar.css"));
 
             // Присвойте EnableOptimizations значение false для отладки. Дополнительные сведения
             // см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
